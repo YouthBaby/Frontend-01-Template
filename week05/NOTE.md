47f34a7d1b67ff383f504a4bf69db1443a31e492
YouthBaby <540969500@qq.com>
2020-05-13 22:26:51 +0800

    第五周作业

--- week05/NOTE.md
+++ week05/NOTE.md
@@ -1,1 +1,133 @@
-# 每周总结可以写在这里
+## Realm
+
+realm在词典上的意思译为领域，可以这么类比的想想。一个realm就是一个小宇宙，每个小宇宙之间的任何事物都是不相等的。
+
+### realm中所有的内置对象
+
+```js
+let objects = [
+  "eval",
+  "isFinite",
+  "isNaN",
+  "parseFloat",
+  "parseInt",
+  "decodeURI",
+  "decodeURIComponent",
+  "encodeURI",
+  "encodeURIComponent",
+  "Array",
+  "Date",
+  "RegExp",
+  "Promise",
+  "Proxy",
+  "Map",
+  "WeakMap",
+  "Set",
+  "WeakSet",
+  "Function",
+  "Boolean",
+  "String",
+  "Number",
+  "Symbol",
+  "Object",
+  "Error",
+  "EvalError",
+  "RangeError",
+  "ReferenceError",
+  "SyntaxError",
+  "TypeError",
+  "URIError",
+  "ArrayBuffer",
+  "SharedArrayBuffer",
+  "DataView",
+  "Float32Array",
+  "Float64Array",
+  "Int8Array",
+  "Int16Array",
+  "Int32Array",
+  "Uint8Array",
+  "Uint16Array",
+  "Uint32Array",
+  "Uint8ClampedArray",
+  "Atomics",
+  "JSON",
+  "Math",
+  "Reflect"
+];
+```
+
+具体解析代码见[realm.js](./realm.js)
+
+
+
+## 状态机实现HTTP request和reponse解析
+
+
+
+### Reuest解析
+
+![request](./static/request.png)
+
+请求报文大致分为：请求行、请求头、请求体三部分。
+
+由此我们可以构造以下结构体
+
+![parser](/Users/zhangjiazhen/projects/Frontend-01-Template/week05/static/request-parser.jpg)
+
+此时我们可以构造一个toString函数来构造我们的请求报文
+
+```js
+toString() {
+    return `${this.method} ${this.path} HTTP/1.1\r
+${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}
+\r
+${this.bodyText}`
+}
+```
+
+请注意，此处不能缩进，否则会报错。这时，我们可以通过net模块建立TCP连接，可以开始发请求了。
+
+## Response解析
+
+![response](/Users/zhangjiazhen/projects/Frontend-01-Template/week05/static/response.png)
+
+响应报文与请求报文的结构类似，也大致分为三部分：响应行、响应头、响应体。
+
+响应体是 length content 循环，最后一个length 一定为 0
+
+由于响应体的内容跟其他部分耦合度不高，我们可以分开解析。
+
+响应解析状态定义
+
+```js
+constructor() {
+    this.WATING_STATUS_LINE = 0;
+    this.WATING_STATUS_LINE_END = 1;
+    this.WATING_HEADER_NAME = 2;
+    this.WATING_HEADER_SPACE = 3;
+    this.WATING_HEADER_VALUE = 4;
+    this.WATING_HEADER_LINE_END = 5;
+    this.WATING_HEADER_BLOCK_END = 6;
+    this.WATING_BODY = 7;
+    this.current = this.WATING_STATUS_LINE;
+    this.statusLine = '';
+    this.headers = {};
+    this.headerName = '';
+    this.headerValue = '';
+    this.bodyParser = null;
+}
+```
+
+接下来是单字符处理、主要思路就是根据\r \n两个字符来转换状态，写入变量。
+
+请求体的解析机制与此类似，思路是相同的。
+
+具体代码详见 [client.js](./client.js)
+
+
+
+
+
+## 总结
+
+HTTP是超文本传输协议，请求与响应都是基于文本的。今天才恍然大悟，原来HTTP就是处理各种文本的协议，传输的就是各种各样的文本。
---
+++ week05/client.js
@@ -0,0 +1,203 @@
+const net = require('net');
+
+class Request {
+  constructor(options) {
+    this.method = options.method || 'GET';
+    this.host = options.host;
+    this.path = options.path || '/';
+    this.port = options.port || 80;
+    this.body = options.body || {};
+    this.headers = options.headers || {};
+    if (!this.headers['Content-Type']) {
+      this.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    }
+    if (this.headers['Content-Type'] === 'application/json') {
+      this.bodyText = JSON.stringify(this.body);
+    } else if (this.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+      this.bodyText = Object.keys(this.body).map(
+        key => `${key}=${encodeURIComponent(this.body[key])}`
+      ).join('&');
+    }
+    this.headers['Content-Length'] = this.bodyText.length;
+  }
+
+  toString() {
+    return `${this.method} ${this.path} HTTP/1.1\r
+${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}
+\r
+${this.bodyText}`
+  }
+
+  send(connection) {
+    return new Promise((resove, reject) => {
+      const parser = new ResponseParser();
+      if (connection) {
+        connection.write(this.toString());
+      } else {
+        connection = net.createConnection({
+          host: this.host,
+          port: this.port
+        }, () => {
+          console.log('connect to server');
+          connection.write(this.toString());
+        })
+      }
+
+      connection.on('data', (data) => {
+        parser.recive(data.toString());
+        if (parser.isFinished) {
+          resove(parser.response);
+        }
+        connection.end();
+      })
+
+      connection.on('end', (err) => {
+        reject(err);
+        connection.end();
+      });
+
+    })
+  }
+}
+
+class ResponseParser {
+  constructor() {
+    this.WATING_STATUS_LINE = 0;
+    this.WATING_STATUS_LINE_END = 1;
+    this.WATING_HEADER_NAME = 2;
+    this.WATING_HEADER_SPACE = 3;
+    this.WATING_HEADER_VALUE = 4;
+    this.WATING_HEADER_LINE_END = 5;
+    this.WATING_HEADER_BLOCK_END = 6;
+    this.WATING_BODY = 7;
+    this.current = this.WATING_STATUS_LINE;
+    this.statusLine = '';
+    this.headers = {};
+    this.headerName = '';
+    this.headerValue = '';
+    this.bodyParser = null;
+  }
+  get isFinished() {
+    return this.bodyParser && this.bodyParser.isFinished;
+  }
+  get response() {
+    this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/)
+    return {
+      statusCode: RegExp.$1,
+      statusText: RegExp.$2,
+      headers: this.headers,
+      body: this.bodyParser.content.join('')
+    }
+  }
+  recive(string) {
+    for (let i = 0; i < string.length; i++) {
+      this.reciveChar(string.charAt(i));
+    }
+  }
+  reciveChar(char) {
+    if (this.current === this.WATING_STATUS_LINE) {
+      if (char === '\r') {
+        this.current = this.WATING_STATUS_LINE_END;
+      } else {
+        this.statusLine += char;
+      }
+    } else if (this.current === this.WATING_STATUS_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WATING_HEADER_NAME;
+      }
+    } else if (this.current === this.WATING_HEADER_NAME) {
+      if (char === ':') {
+        this.current = this.WATING_HEADER_SPACE;
+      } else if (char === '\r') {
+        this.current = this.WATING_HEADER_BLOCK_END;
+        if (this.headers['Transfer-Encoding'] === 'chunked') {
+          this.bodyParser = new TrunkedBodyParser();
+        }
+      } else {
+        this.headerName += char;
+      }
+    } else if (this.current === this.WATING_HEADER_SPACE) {
+      if (char === ' ') {
+        this.current = this.WATING_HEADER_VALUE;
+      }
+    } else if (this.current === this.WATING_HEADER_VALUE) {
+      if (char === '\r') {
+        this.current = this.WATING_HEADER_LINE_END;
+        this.headers[this.headerName] = this.headerValue;
+        this.headerName = '';
+        this.headerValue = '';
+      } else {
+        this.headerValue += char;
+      }
+    } else if (this.current === this.WATING_HEADER_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WATING_HEADER_NAME;
+      }
+    } else if (this.current === this.WATING_HEADER_BLOCK_END) {
+      if (char === '\n') {
+        this.current = this.WATING_BODY
+      }
+    } else if (this.current === this.WATING_BODY) {
+      this.bodyParser.reciveChar(char);
+    }
+  }
+}
+
+class TrunkedBodyParser {
+  constructor() {
+    this.WATING_LENGTH = 0;
+    this.WATING_LENGTH_LINE_END = 1;
+    this.READING_TRUNK = 2;
+    this.WATING_NEW_LINE = 3;
+    this.WATING_NEW_LINE_END = 4;
+    this.length = 0;
+    this.isFinished = false;
+    this.content = [];
+    this.current = this.WATING_LENGTH;
+  }
+  reciveChar(char) {
+    if (this.current === this.WATING_LENGTH) {
+      if (char === '\r') {
+        if (this.length === 0) {
+          this.isFinished = true;
+        }
+        this.current = this.WATING_LENGTH_LINE_END;
+      } else {
+        this.length *= 10;
+        this.length += char.charCodeAt(0) - '0'.charCodeAt(0);
+      }
+    } else if (this.current === this.WATING_LENGTH_LINE_END) {
+      if (char === '\n') {
+        this.current = this.READING_TRUNK;
+      }
+    } else if (this.current === this.READING_TRUNK) {
+      this.content.push(char);
+      if (--this.length === 0) {
+        this.current = this.WATING_NEW_LINE;
+      }
+    } else if (this.current === this.WATING_NEW_LINE) {
+      if (char === '\r') {
+        this.current = this.WATING_NEW_LINE_END;
+      }
+    } else if (this.current === this.WATING_NEW_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WATING_LENGTH;
+      }
+    }
+  }
+}
+
+void async function() {
+  const request = new Request({
+    host: '127.0.0.1',
+    port: 8088,
+    path: '/',
+    method: 'POST',
+    body: {
+      name: 'winter'
+    }
+  })
+
+  let response = await request.send();
+  console.log(response);
+}()
